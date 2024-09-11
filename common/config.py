576584213409061d6dbaaa2876c8179de25ce231
Johannes Koester <johannes.koester@uni-due.de>
2024-09-11 12:48:37 +0200

    implement srptn config as singleton

--- common/config.py
+++ common/config.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from pathlib import Path
+from dataclasses import dataclass, field
 from typing import List
 from dataclasses_json import dataclass_json
 
@@ -6,4 +7,13 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass
 class Config:
-    pass
+    singleton: Config = field(init=False, default=None)
+
+
+    workdir: Path
+
+    @classmethod
+    def __new__(cls):
+        if cls.singleton is None:
+            cls.singleton = Config.from_json(os.environ.get("SRPTN_CONFIG"))
+        return cls.singleton
